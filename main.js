bb379ae291df3596af2fa1ee0a6ff9b47016e85d
Raul <lokoconbotas@gmail.com>
2017-06-30 16:28:16 +0200

    Function to make table Added

--- main.js
+++ main.js
@@ -38,3 +38,33 @@ function getPosts() {
     });
 
 }
+/*  <div class="chats">
+      <div class="chat">
+        <h3 class="chatName">Nombre Chat</h3>
+        <div class="logoContainer">
+          <img class='logo' src="icons/escudo1.png" alt="thumbnail">
+        </div>
+        <div class="messages"></div>
+        <div class="postMessage"></div>
+        <button class="sendButton" value='Post'>Post Message</button>
+      </div>*/
+
+createTable(21);
+function createTable(data){
+    var chats = $('.chats');
+    for (var i = 0; i < data; i++) {
+        var element = data[i];
+        var $chat = $('<div/>').addClass('chat');
+        var $chatName = $('<h3/>').addClass('chatName').html('Match');
+        var $logoContainer = $('<div/>').addClass('logoContainer');
+        var $logoContainer = $('<div/>').addClass('logoContainer');
+        var $logo = $('<img>').addClass('logo').attr('src', 'icons/escudo' + (i+1) + '.png');
+        var $messages = $('<div/>').addClass('messages');
+        var $postMessage = $('<div/>').addClass('postMessage'); 
+        var $button = $('<button/>').addClass('sendButton').html('Post Message');
+
+        $logoContainer.append($logo);
+        $chat.append($chatName, $logoContainer, $messages, $postMessage, $button);
+        chats.append($chat);
+    }
+}
